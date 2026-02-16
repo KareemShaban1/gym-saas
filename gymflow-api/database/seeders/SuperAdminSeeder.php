1ed539c8260d3277c760689004e4a9835843f371
KareemShaban1 <karim17196@fci.bu.edu.eg>
2026-02-16 11:14:09 +0200

    make new updates

--- gymflow-api/database/seeders/SuperAdminSeeder.php
+++ gymflow-api/database/seeders/SuperAdminSeeder.php
@@ -1,1 +1,26 @@
+<?php
 
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $email = env('SUPER_ADMIN_EMAIL', 'superadmin@gymflow.com');
+        $password = env('SUPER_ADMIN_PASSWORD', 'password');
+
+        User::updateOrCreate(
+            ['email' => $email],
+            [
+                'name' => env('SUPER_ADMIN_NAME', 'Super Admin'),
+                'password' => Hash::make($password),
+                'role' => 'super_admin',
+                'gym_id' => null,
+            ]
+        );
+    }
+}
